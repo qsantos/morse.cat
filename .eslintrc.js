c913769e75b443285c71e4b8403dd822019d46be
Quentin Santos <qsantos@qsantos.fr>
2023-04-25 19:55:06 +0200

    Customize ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: [
+            'error',
+            4,
+        ],
+        'space-before-function-paren': ['error', 'never'],
+        'no-else-return': ['off'],
+    },
 };
