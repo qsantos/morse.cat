bd291c0089c27ac3b4e580cc2a8c728d69e023b9
Quentin Santos <qsantos@qsantos.fr>
2024-09-14 19:26:50 +0200

    Remove unneeded type hint

--- src/lib.js
+++ src/lib.js
@@ -14,7 +14,6 @@ let inSession = false;
 let sessionId = '';
 /** @type {Date} */
 let sessionStart;
-/** @type {number} */
 let sessionDurationUpdater = 0;
 
 /** @type {keyof typeof translations} */
