7611d89107d9ec5263e4d7468d529d16723bb489
Quentin Santos <qsantos@qsantos.fr>
2024-10-07 21:49:28 +0200

    Don't make cwPlayer a global

--- src/contest.js
+++ src/contest.js
@@ -1,10 +1,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-let cwPlayer = undefined;
-
 function cq() {
-    cwPlayer = new jscw();
+    const cwPlayer = new jscw();
     cwPlayer.q = 13;
     cwPlayer.setText('CQCQ TEST G4FON');
     cwPlayer.setWpm(30);
