3b0b673e543453f5a0ba68c96e5c942ea54e20b3
Quentin Santos <qsantos@qsantos.fr>
2024-10-07 21:49:21 +0200

    Remove duplicate line

--- src/contest.js
+++ src/contest.js
@@ -6,7 +6,6 @@ let cwPlayer = undefined;
 function cq() {
     cwPlayer = new jscw();
     cwPlayer.q = 13;
-    cwPlayer.q = 13;
     cwPlayer.setText('CQCQ TEST G4FON');
     cwPlayer.setWpm(30);
     cwPlayer.setEff(30);
