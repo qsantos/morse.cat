820d45e93b936d13793c299ad1b973f461f138e6
Quentin Santos <qsantos@qsantos.fr>
2024-10-07 21:51:55 +0200

    Allow multiple signals at the same time

--- src/contest.js
+++ src/contest.js
@@ -2,16 +2,23 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 function cq() {
+    // set up cwPlayer
     const cwPlayer = new jscw();
     cwPlayer.q = 13;
     cwPlayer.setText('CQCQ TEST G4FON');
     cwPlayer.setWpm(30);
     cwPlayer.setEff(30);
     cwPlayer.setFreq(600);
+
+    // set up decoder
+    const log = document.getElementById("log");
+    const logEntry = document.createElement("LI");
+    log.appendChild(logEntry);
+
     let played = [""];
     cwPlayer.onCharacterPlay = (c) => {
         played.push(c.c);
-        document.getElementById("log").innerHTML = played.join("").toUpperCase();
+        logEntry.innerHTML = played.join("").toUpperCase();
     };
     cwPlayer.play();
 }
