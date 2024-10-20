1326ed8aa460cfaf20baaa11d9f0e49572da1d14
Quentin Santos <qsantos@qsantos.fr>
2024-10-20 08:57:01 +0200

    Make sendMorse() return a Promise

--- src/contest.js
+++ src/contest.js
@@ -2,36 +2,43 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 function sendMorse(sender, message) {
-    // Set up cwPlayer
-    const cwPlayer = new jscw();
-    cwPlayer.q = 13;
-    cwPlayer.setText(message);
-    cwPlayer.setWpm(30);
-    cwPlayer.setEff(30);
-    cwPlayer.setFreq(600);
+    return new Promise((accept) => {
+        // Set up cwPlayer
+        const cwPlayer = new jscw();
+        cwPlayer.q = 13;
+        cwPlayer.setText(message);
+        cwPlayer.setWpm(30);
+        cwPlayer.setEff(30);
+        cwPlayer.setFreq(600);
 
-    // Log Entry
-    const log = document.getElementById("log");
-    const logEntry = document.createElement("LI");
-    log.appendChild(logEntry);
+        // Log Entry
+        const log = document.getElementById("log");
+        const logEntry = document.createElement("LI");
+        log.appendChild(logEntry);
 
-    // Sender label
-    const callSign = document.getElementById("their-call-sign").value;
-    const callSignLabel = document.createElement("SPAN");
-    callSignLabel.innerHTML = `${sender}: `;
-    logEntry.appendChild(callSignLabel);
+        // Sender label
+        const callSign = document.getElementById("their-call-sign").value;
+        const callSignLabel = document.createElement("SPAN");
+        callSignLabel.innerHTML = `${sender}: `;
+        logEntry.appendChild(callSignLabel);
 
-    // Decoded Morse
-    const decodedMorse = document.createElement("SPAN");
-    logEntry.appendChild(decodedMorse);
-    let played = [""];
-    cwPlayer.onCharacterPlay = (c) => {
-        played.push(c.c);
-        decodedMorse.innerHTML = played.join("").toUpperCase();
-    };
+        // Decoded Morse
+        const decodedMorse = document.createElement("SPAN");
+        logEntry.appendChild(decodedMorse);
+        let played = [""];
+        cwPlayer.onCharacterPlay = (c) => {
+            played.push(c.c);
+            decodedMorse.innerHTML = played.join("").toUpperCase();
+        };
 
-    // Start
-    cwPlayer.play();
+        // Promise handling
+        cwPlayer.onFinished = () => {
+            accept();
+        };
+
+        // Start
+        cwPlayer.play();
+    });
 }
 
 function cq() {
